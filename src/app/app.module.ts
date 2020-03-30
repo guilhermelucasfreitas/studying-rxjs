4b7c7a39fe036da54cd8a5659b9648e4c6c371e0
Guilherme Pereira <guilherme.pereira@hubbrasil.local>
2020-03-30 12:55:42 -0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BasicCreationComponent } from './basic-creation/basic-creation.component';
+import { MaterialModule } from './material.module';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BasicCreationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/basic-creation/basic-creation.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, Observer, from, of, interval, timer, Subscription, fromEvent } from 'rxjs';
+
+@Component({
+  selector: 'basic-creation',
+  templateUrl: './basic-creation.component.html',
+  styleUrls: ['./basic-creation.component.css']
+})
+export class BasicCreationComponent implements OnInit {
+
+  constructor() { }
+
+  inscricao: Subscription = new Subscription();
+
+  ngOnInit() {
+  }
+
+  observableCreate(){
+    const hello = Observable.create((observer: Observer<string>) => {
+      observer.next("hello");
+      observer.next("from");
+      observer.next("observable");
+      observer.complete();
+    })
+
+    hello.subscribe(val => console.log(val));
+  }
+
+  //cria um observable e imprime cada posição no array, 1 por 1
+  fromClick(){
+    from([1,2,3,4,5,{x:10, y:20}])
+      .subscribe((v) => console.log(v));
+  }
+
+  //cria um observable e imprime em um objeto só
+  ofClick() {
+    of([1,2,3,4,5,{x:10, y:20}])
+      .subscribe((v) => console.log(v));
+  }
+
+  //cria um observable que chama o método a cada 1 segundo
+  intervalClick(){
+    const source = interval(1000);
+    //desinscrevendo
+    const subscription = source.subscribe((v) => console.log(v));
+    this.inscricao.add(subscription)
+  }
+
+  timerClick(){
+    //parecido com o interval, possivel que ele funcione apenas depois de 1 segundo e depois disso ele para
+    const source = timer(1000)
+    //começa esperando 3 segundos e depois continua a cada 1 segundo
+    //const source = timer(3000,1000)
+    const subscription = source.subscribe((v) => console.log(v));
+    this.inscricao.add(subscription)
+  }
+
+  unsubscripeClick(){
+    //para todos os observables
+    this.inscricao.unsubscribe();
+    this.inscricao = new Subscription();
+  }
+
+  fromEventClick(){
+    const subscription = fromEvent(document, 'click')
+      .subscribe((e) => console.log(e));
+      this.inscricao.add(subscription)
+  }
+}
